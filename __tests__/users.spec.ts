d2e61345c60713b8d892dcdb51aa809327250ff8
fe-ercg <mabfher@gmail.com>
2024-09-18 18:37:20 -0500

    feat: add token for test

--- __tests__/users.spec.ts
+++ __tests__/users.spec.ts
@@ -1,13 +1,22 @@
 import request  from "supertest";
 import app from "../src/app";
-// import { describe } from "node:test";
+import jwt from "jsonwebtoken";
+
+const JWT_KEY = 'admin';
 
 describe("e2e USERS CRUD", ()=> {
     let createdUserId: string;
 
+    let token: string;
+
+    beforeEach(()=> {
+        token = jwt.sign({username: 'Admin'}, JWT_KEY, {expiresIn: '1h'});
+    })
+
     it('should create a new user successfully', async () => {
     const res = await request(app)
         .post('/users')
+        .set('Authorization', `Bearer ${token}`)
         .send({
             name: 'Jack cats',
             email: 'jack.c@meows.com',
@@ -25,6 +34,7 @@ describe("e2e USERS CRUD", ()=> {
     it('should update the user information', async () => {
     const res = await request(app)
         .patch(`/users/${createdUserId}`)
+        .set('Authorization', `Bearer ${token}`)
         .send({
         name: 'Jackson'
         });
@@ -35,7 +45,9 @@ describe("e2e USERS CRUD", ()=> {
     });
 
     it('should retrieve the list of users including the newly created user', async () => {
-    const res = await request(app).get('/users');
+    const res = await request(app)
+        .get('/users')
+        .set('Authorization', `Bearer ${token}`)
     
     expect(res.status).toBe(200);
     expect(Array.isArray(res.body)).toBe(true);
@@ -47,7 +59,8 @@ describe("e2e USERS CRUD", ()=> {
 
     it('should delete the created user', async () => {
     const res = await request(app)
-        .delete(`/users/${createdUserId}`);
+        .delete(`/users/${createdUserId}`)
+        .set('Authorization', `Bearer ${token}`)
     
     expect(res.status).toBe(200);
     expect(res.body).toHaveProperty('id');
@@ -56,7 +69,8 @@ describe("e2e USERS CRUD", ()=> {
 
     it('should return 404 when trying to get the deleted user', async () => {
     const res = await request(app)
-        .get(`/users/${createdUserId}`);
+    .get(`/users/${createdUserId}`)
+    .set('Authorization', `Bearer ${token}`)
     
     expect(res.status).toBe(404);
     });
